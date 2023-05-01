1189fd6504bb7ccd7352c0e75c8a73d50b8d068b
Meet Jain <94193229+meetpunmiya@users.noreply.github.com>
2023-05-01 18:07:09 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+let movies = [
+    {
+        image: 'img/banner/baner1.webp'
+    },
+    {
+        image: 'img/banner/banner2.webp'
+    },
+    {
+        image: 'img/banner/banner3.webp'
+    },
+    {
+        image: 'img/banner/banner4.webp'
+    },
+    {
+        image: 'img/banner/banner5.webp'
+    },
+    {
+        image: 'img/banner/banner6.webp'
+    }
+    {
+        image: '"C:\Users\Admin\OneDrive\Desktop\BMS project\img\logo.png"'
+    }
+]
